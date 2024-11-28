eb5457a9dba973263d0903b1f111389426a19ca9
Aryan Tele <telearyan12@gmail.com>
2024-11-28 20:48:19 +0530

    project setup and login page built

---
+++ components/auth/login-button.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+}
+
+export const LoginButton = ({
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) => {
+  const onClick = () => {
+    console.log("LOGIN BUTTON CLICKED");
+  };
+
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+};
