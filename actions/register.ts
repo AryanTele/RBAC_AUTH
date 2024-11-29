ef62c40e7e0c3ebc2abeddb48944d6b67d4d02fc
Aryan Tele <telearyan12@gmail.com>
2024-11-29 17:45:03 +0530

    register user to neon and hash password

--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,30 @@
 "use server";
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
-
+import { db } from "@/lib/db";
+import bcrypt from "bcrypt";
+import { getUserByEmail } from "@/data/user";
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   // validate at server
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: "Invalid fields !" };
   }
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-  return { success: "Email sent!" };
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return { error: "Email is already taken" };
+  }
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // sent verification token
+  return { success: "User created!" };
 };
