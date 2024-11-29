87b4dc4e3478853bd104f46c4cfd4f735deba7c6
Aryan Tele <telearyan12@gmail.com>
2024-11-29 23:40:41 +0530

    implement logout

--- actions/login.ts
+++ actions/login.ts
@@ -1,7 +1,9 @@
 "use server";
 import * as z from "zod";
 import { LoginSchema } from "@/schemas";
-
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/route";
+import { AuthError } from "next-auth";
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   // validate at server
   const validatedFields = LoginSchema.safeParse(values);
@@ -9,5 +11,22 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: "Invalid fields !" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password } = validatedFields.data;
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid Credentials" };
+        default:
+          return { error: "Something went wrong" };
+      }
+    }
+    throw error;
+  }
 };
