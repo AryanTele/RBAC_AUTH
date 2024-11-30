8f1367a5c06d7d0582690480bd0740f3c41e5a4f
Aryan Tele <telearyan12@gmail.com>
2024-11-30 13:36:59 +0530

    setup oauth for google and github

--- auth.config.ts
+++ auth.config.ts
@@ -2,9 +2,19 @@ import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
 import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import bcrypt from "bcryptjs";
 export default {
   providers: [
+    Github({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validateFields = LoginSchema.safeParse(credentials);
