82ff42fc70dd23f4873ede845a3e904ea629ed13
Aryan Tele <telearyan12@gmail.com>
2024-12-02 18:30:05 +0530

    feature: test reset email feature

--- actions/reset.ts
+++ actions/reset.ts
@@ -2,7 +2,8 @@
 import { ResetSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import * as z from "zod";
-
+import { sendPasswordResetEmail } from "@/lib/mail";
+import { generatePasswordResetToken } from "@/lib/tokens";
 export const reset = async (values: z.infer<typeof ResetSchema>) => {
   const validatedFields = ResetSchema.safeParse(values);
   if (!validatedFields.success) {
@@ -13,6 +14,11 @@ export const reset = async (values: z.infer<typeof ResetSchema>) => {
   if (!existingUser) {
     return { error: "Email not found" };
   }
+  const passwordResetToken = await generatePasswordResetToken(email);
+  await sendPasswordResetEmail(
+    passwordResetToken.email,
+    passwordResetToken.token
+  );
 
   return { success: "Reset email sent!" };
 };
